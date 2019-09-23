edfa170765a28a7b26ffb747f44a1000a9b52177
Nursultan <abdyldaev93@gmail.com>
2019-09-23 12:26:51 -0700

    output request object

--- intents/continue_service_request.rb
+++ intents/continue_service_request.rb
@@ -1,7 +1,8 @@
-require 'byebug'
-
 intent 'ContinueServiceRequest' do
   answer = request.slot_value("answer")
+
+  puts request
+  puts '---------------------'
 
   if answer == 'yes'
     ask('OK, your location is 2437 Barry Avenue, Los Angeles, California.')
